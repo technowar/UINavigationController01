4d6f89c04810af11de70a75535656416ba41e947
Johmel Rey Pintor <jahmgwapo@skolpad.org>
2013-06-09 17:08:34 +0800

    Added the second view controller.

---
+++ UINavigationController01/NextPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  NextPageViewController.h
+//  UINavigationController01
+//
+//  Created by OSX on 6/9/13.
+//  Copyright (c) 2013 OSX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NextPageViewController : UIViewController
+
+@end
---
+++ UINavigationController01/NextPageViewController.m
@@ -0,0 +1,38 @@
+//
+//  NextPageViewController.m
+//  UINavigationController01
+//
+//  Created by OSX on 6/9/13.
+//  Copyright (c) 2013 OSX. All rights reserved.
+//
+
+#import "NextPageViewController.h"
+
+@interface NextPageViewController ()
+
+@end
+
+@implementation NextPageViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

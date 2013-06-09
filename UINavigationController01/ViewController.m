2fdbc106e3e142fd03df4c79d5c2fa7a6fd1ce81
Johmel Rey Pintor <jahmgwapo@skolpad.org>
2013-06-09 16:53:17 +0800

    Made the UINavigationBar not visible in the first view controller.

--- UINavigationController01/ViewController.m
+++ UINavigationController01/ViewController.m
@@ -26,4 +26,16 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)viewWillAppear:(BOOL)animated
+{
+    [self.navigationController setNavigationBarHidden:YES animated:NO];
+    [super viewWillAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [self.navigationController setNavigationBarHidden:NO animated:NO];
+    [super viewWillDisappear:animated];
+}
+
 @end

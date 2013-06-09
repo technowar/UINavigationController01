b6038c22027f18a3877c4af0b3915d1e19ccf56b
Johmel Rey Pintor <jahmgwapo@skolpad.org>
2013-06-09 17:13:48 +0800

    Created own button in the UINavigationBar. Used popViewController.

--- UINavigationController01/NextPageViewController.m
+++ UINavigationController01/NextPageViewController.m
@@ -26,13 +26,25 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    
+    UIBarButtonItem *leftBarButton = [[UIBarButtonItem alloc] initWithTitle: @"Cancel"
+                                                                      style:UIBarButtonItemStylePlain
+                                                                     target:self
+                                                                     action:@selector(cancelButtonPressed)];
+    self.navigationItem.leftBarButtonItem =leftBarButton;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)cancelButtonPressed
+{
+    [self.navigationController popViewControllerAnimated:YES];
+    
+    NSLog(@"Cancel");
 }
 
 @end

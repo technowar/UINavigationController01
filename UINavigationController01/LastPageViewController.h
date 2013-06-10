2174eafc9b8f8ae3f31a2011985f8450a7c1ae07
Johmel Rey Pintor <jahmgwapo@skolpad.org>
2013-06-10 15:45:43 +0800

    Added a view controller.

---
+++ UINavigationController01/LastPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  LastPageViewController.h
+//  UINavigationController01
+//
+//  Created by OSX on 6/10/13.
+//  Copyright (c) 2013 OSX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LastPageViewController : UIViewController
+
+@end
---
+++ UINavigationController01/LastPageViewController.m
@@ -0,0 +1,52 @@
+//
+//  LastPageViewController.m
+//  UINavigationController01
+//
+//  Created by OSX on 6/10/13.
+//  Copyright (c) 2013 OSX. All rights reserved.
+//
+
+#import "LastPageViewController.h"
+
+@interface LastPageViewController ()
+
+@end
+
+@implementation LastPageViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.view.backgroundColor = [UIColor blueColor];
+    }
+    
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    UIBarButtonItem *leftBarButton = [[UIBarButtonItem alloc] initWithTitle: @"Return"
+                                                                      style:UIBarButtonItemStylePlain
+                                                                     target:self
+                                                                     action:@selector(returnButtonPressed)];
+    self.navigationItem.leftBarButtonItem =leftBarButton;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark -
+#pragma mark Button Properties
+
+- (void)returnButtonPressed
+{
+    
+}
+
+@end
